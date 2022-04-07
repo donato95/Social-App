d77b190ea003de5e54fecddb7e7362ca823a7097
Muhammad A. Donato <48384773+donato95@users.noreply.github.com>
2022-04-07 04:21:11 +0200

    v1

--- app/main/routes.py
+++ app/main/routes.py
@@ -11,3 +11,41 @@ def login():
 @main.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('main/register.html')
+
+
+@main.route('/account/<int:id>', methods=['GET', 'POST'])
+def account(id):
+    id = id
+    return render_template('main/account.html', id=id)
+
+
+@main.route('/account/<int:id>/setting', methods=['GET', 'POST'])
+def setting(id):
+    id = id
+    return render_template('main/setting.html', id=id)
+
+
+@main.route('/account/<int:id>/delete', methods=['GET', 'POST'])
+def delete(id):
+    id = id
+    return render_template('main/account.html', id=id)
+
+
+@main.route('/account/<int:id>/confirm', methods=['GET', 'POST'])
+def confirm(id):
+    id = id
+    return render_template('main/account.html', id=id)
+
+
+@main.route('/account/<int:id>/<string:action>', methods=['GET', 'POST'])
+def action(id, action):
+    id = id
+    action = action
+    if action == 'follow':
+        pass
+    if action == 'unfollow':
+        pass
+    if action == 'block':
+        pass
+    return render_template('main/account.html', id=id, action=action)
+
